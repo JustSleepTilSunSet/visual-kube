f4002345c70dbccaba2ace844341a3751dc485ee
JustSleepTilSunSet <cloverchen.042@gmail.com>
2025-07-05 16:17:48 +0800

    Feature: implement kube ingress and ingress controller.

--- app.js
+++ app.js
@@ -31,7 +31,9 @@ app.use('/users', usersRouter);
 app.use(function (req, res, next) {
   next(createError(404));
 });
-
+app.use('/health',()=>{
+  res.render('index', { version: process.env.VERSION });
+})
 // error handler
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 let kubectl_router = require('./kubecontroller/router')
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express112' });
 });
 router.use('/v1',kubectl_router)
 module.exports = router;
